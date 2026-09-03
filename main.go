c6f11b468290bc7e8c6cd47f049183d24566879e
flames31 <rrahulao2000@gmail.com>
2026-09-02 23:29:30 -0400

    Part 1 - refactored

--- main.go
+++ main.go
@@ -8,22 +8,34 @@ import (
 )
 
 func main() {
-	filename := "problems.csv"
+	filename := getFileName()
 
-	if len(os.Args) > 1 {
-		filename = os.Args[2]
-	}
 	fmt.Println("Welcome to the Quiz!")
+
 	file, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-	reader := csv.NewReader(file)
+
+	score, total := runQuiz(csv.NewReader(file))
+
+	fmt.Printf("You scored: %v point(s) out of %v!\n", score, total)
+}
+
+func getFileName() string {
+	filename := "problems.csv"
+	if len(os.Args) > 1 {
+		filename = os.Args[2]
+	}
+	return filename
+}
+
+func runQuiz(csvReader *csv.Reader) (int, int) {
 	points, questions := 0, 0
 
 	for {
-		record, err := reader.Read()
+		record, err := csvReader.Read()
 		if err == io.EOF {
 			break
 		}
@@ -41,5 +53,5 @@ func main() {
 		questions++
 	}
 
-	fmt.Printf("You scored: %v point(s) out of %v!\n", points, questions)
+	return points, questions
 }
