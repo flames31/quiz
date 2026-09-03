e47c52ea571e425182ab26865acb6c5c26d8bddf
flames31 <rrahulao2000@gmail.com>
2026-09-03 00:01:05 -0400

    Part 2

--- main.go
+++ main.go
@@ -2,56 +2,93 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
-	"io"
 	"os"
+	"time"
 )
 
+type Quiz struct {
+	total     int
+	score     int
+	questions []Question
+}
+
+type Question struct {
+	question string
+	answer   string
+}
+
 func main() {
-	filename := getFileName()
+	filename := flag.String("file", "problems.csv", "file to read")
+	timeInSeconds := flag.Int("time", 60, "time in seconds")
 
 	fmt.Println("Welcome to the Quiz!")
 
-	file, err := os.Open(filename)
+	file, err := os.Open(*filename)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
-	score, total := runQuiz(csv.NewReader(file))
+	quiz := getQuiz(csv.NewReader(file))
 
-	fmt.Printf("You scored: %v point(s) out of %v!\n", score, total)
+	runQuizWithTimer(quiz, *timeInSeconds)
+
+	fmt.Printf("You scored: %v point(s) out of %v!\n", quiz.score, quiz.total)
 }
 
 func getFileName() string {
 	filename := "problems.csv"
-	if len(os.Args) > 1 {
+	if len(os.Args) > 2 && os.Args[1] == "-file" {
 		filename = os.Args[2]
 	}
 	return filename
 }
 
-func runQuiz(csvReader *csv.Reader) (int, int) {
-	points, questions := 0, 0
+func getQuiz(reader *csv.Reader) *Quiz {
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+	quiz := &Quiz{
+		total:     len(records),
+		score:     0,
+		questions: make([]Question, len(records)),
+	}
 
-	for {
-		record, err := csvReader.Read()
-		if err == io.EOF {
-			break
+	for i, record := range records {
+		quiz.questions[i] = Question{
+			question: record[0],
+			answer:   record[1],
 		}
-		if err != nil {
-			panic(err)
-		}
-		q, a := record[0], record[1]
-		fmt.Println("Question: ", q)
+	}
+	return quiz
+}
+
+func runQuiz(quiz *Quiz) {
+	for _, question := range quiz.questions {
+		fmt.Println("Question: ", question.question)
 		var input string
 		fmt.Scanln(&input)
 
-		if a == input {
-			points++
+		if question.answer == input {
+			quiz.score++
 		}
-		questions++
 	}
+}
 
-	return points, questions
+func runQuizWithTimer(quiz *Quiz, timeInSeconds int) {
+
+	done := make(chan bool, 1)
+	go func() {
+		runQuiz(quiz)
+		done <- true
+	}()
+	select {
+	case <-done:
+		fmt.Println("Quiz finished!")
+	case <-time.After(time.Duration(timeInSeconds) * time.Second):
+		fmt.Println("Time is up!")
+	}
 }
